24da2890ce9d5fceb2a7bde7e5f05c0f000a21d8
freshfish1080 <45006160+freshfish1080@users.noreply.github.com>
2022-08-02 15:31:03 +0900

    gps-test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState } from 'react';
 import './App.css';
 
+// import { GpsProvider } from './providers/GpsProvider';
+// import GpsButton from './components/GpsButton';
+
 function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [isPushed, setIsPushed] = useState(false);
+
+  const successCallback = (position:GeolocationPosition) => {
+    console.log(position);
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+    setIsPushed(true);
+  }
+  const errorCallback = (position:GeolocationPositionError) => {
+    console.log(position);
+  }
+  // const onClickGetLocation = () => {    
+  //   // navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  //   navigator.geolocation.watchPosition(successCallback, errorCallback);
+  // }
+  // Geolocation APIに対応していない時
+  if (!navigator.geolocation) {    
+    alert("この端末では位置情報が取得できません");  
+  } 
+  navigator.geolocation.watchPosition(successCallback, errorCallback);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <GpsProvider>                       */}
+        {/* <button onClick={onClickGetLocation}>GPS取得</button> */}
+          {/* {isPushed ? <div>{latitude}</div> : <div></div>}
+          {isPushed ? <div>{longitude}</div> : <div></div>} */}
+          <div>緯度:{latitude} 経度:{longitude}</div>
+      {/* </GpsProvider> */}
     </div>
   );
 }
